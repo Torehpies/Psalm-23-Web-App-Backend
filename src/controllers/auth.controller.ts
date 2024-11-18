9b010ba80d85a7456eae495cdfc696726554970a
Torehpies <salupadoseiffer@gmail.com>
2024-11-18 18:36:18 +0800

    Added JWT Token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import Role from "../models/Role";
 import User from "../models/User";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { CreateError } from '../utils/error';
 import { CreateSuccess } from '../utils/success';
 
@@ -25,15 +26,32 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const user = await User.findOne({ email: req.body.email });
+		const user = await User.findOne({ email: req.body.email })
+		.populate("roles", "role");
+
 		if (!user) {
 			return next(CreateError(404, "User not found"));
 		}
+		
+		const { roles } = user;
+
 		const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
 		if(!isPasswordCorrect){
 			return next(CreateError(400, "Password is incorrect"));
 		}
-		return next(CreateSuccess(200, "Login Succcess"));
+
+		const token = jwt.sign(
+			{id: user._id, isAdmin: user.isAdmin, roles: roles},
+			process.env.JWT_SECRET as string,
+		)
+
+		res.cookie("access_token", token, {httpOnly: true})
+		.status(200)
+		.json({
+				status: 200,
+				message: "Login Success",
+				data: user
+			})
 	} catch (error) {
 		return next(CreateError(500, "Something went wrong"));
 	}

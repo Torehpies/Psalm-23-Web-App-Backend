f5af11e11be2dcc990f2cb686205c50215c445e2
mikeru <127611876+mikeruC@users.noreply.github.com>
2024-12-09 22:24:32 +0800

    what

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createOrder, getOrders, getOrderById, updateOrder, deleteOrder } from "../controllers/orders.controller";
+import { updateOrderPerformance } from "../controllers/OrderPerformance.controller";
 
 const router = Router();
 
@@ -8,5 +9,6 @@ router.get("/", getOrders);
 router.get("/:id", getOrderById);
 router.put("/:id", updateOrder);
 router.delete("/:id", deleteOrder);
+router.post("/performance", updateOrderPerformance);
 
 export default router;

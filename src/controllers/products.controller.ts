d72f3770dc03b7ee5a1452c15a6908977a988c0b
Torehpies <salupadoseiffer@gmail.com>
2024-12-12 13:09:03 +0800

    Updated products controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,48 +1,50 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Products from "../models/Product";
+import { CreateSuccess } from "../utils/success";
+import { CreateError } from "../utils/error";
 
-export const getAllProducts = async (_req: Request, res: Response) => {
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const products = await Products.find({});
-        res.status(200).send(products);
+        return next(CreateSuccess(200, "All Products fetched", products));
     } catch (error) {
-        res.status(500).send(error instanceof Error ? error.message : "Unknown error");
+        return next(CreateError(500, "Internal Server Error"));
     }
 };
 
-export const getAllCategories = async (_req: Request, res: Response) => {
+export const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const categories = await Products.distinct("unit");
-        res.status(200).send(categories);
+        return next(CreateSuccess(200, "All Categories fetched", categories));
     } catch (error) {
-        res.status(500).send(error instanceof Error ? error.message : "Unknown error");
+        return next(CreateError(500, "Internal Server Error"));
     }
 };
 
-export const getProductById = async (req: Request, res: Response) => {
+export const getProductById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const product = await Products.findById(req.params.id);
         if (!product) {
-            res.status(404).send("Product not found");
+            return next(CreateError(404, "Product not found"));
         } else {
-            res.status(200).send(product);
+            return next(CreateSuccess(200, "Product fetched", product));
         }
     } catch (error) {
-        res.status(500).send(error instanceof Error ? error.message : "Unknown error");
+        return next(CreateError(500, "Internal Server Error"));
     }
 };
 
-export const createProduct = async (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const newProduct = new Products(req.body);
         await newProduct.save();
         res.status(201).send(`Created a new product: ID ${newProduct._id}`);
     } catch (error) {
-        res.status(400).send(error instanceof Error ? error.message : "Unknown error");
+        return next(CreateError(500, "Internal Server Error"));
     }
 };
 
-export const updateProduct = async (req: Request, res: Response) => {
+export const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const updatedProduct = await Products.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!updatedProduct) {
@@ -51,11 +53,11 @@ export const updateProduct = async (req: Request, res: Response) => {
             res.status(200).send(`Updated product: ID ${updatedProduct._id}`);
         }
     } catch (error) {
-        res.status(400).send(error instanceof Error ? error.message : "Unknown error");
+        return next(CreateError(400, "Unknown Error"));
     }
 };
 
-export const deleteProduct = async (req: Request, res: Response) => {
+export const deleteProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const result = await Products.findByIdAndDelete(req.params.id);
         if (!result) {
